20600fc45156d870a39dc715d9a7967d1275f8c6
YTMT-1 <m71589488@gmail.com>
2026-07-26 15:42:14 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,6 @@
 local BaseUrl = "https://raw.githubusercontent.com/YTMT-1/idk/refs/heads/main/"
-if game.PlaceId ~= 6516141723 or game.PlaceId ~= 1785036888943 then
+
+-- Nếu người chơi đang ở game Doors HOẶC game phụ thì mới tải script
+if game.PlaceId == 6516141723 or game.PlaceId == 1785036888943 then
     loadstring(game:HttpGet(BaseUrl .. "ye.luau"))()
 end
