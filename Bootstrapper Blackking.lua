16788c941ae659b2fd2f27394d780d68e2c0ad23
YTMT-1 <m71589488@gmail.com>
2026-08-12 19:03:44 +0700

    Update Bootstrapper Blackking.lua

--- Bootstrapper Blackking.lua
+++ Bootstrapper Blackking.lua
@@ -1,6 +1,5 @@
-local script_code = [[
 if getgenv().BlackKing then
-    getgenv().BlackKing = nil
+    return
 end
 
 -- --- BẮT ĐẦU SCRIPT GỐC ---
@@ -96,14 +95,3 @@ task.wait(0.5)
 
 Loading:Continue()
 print("Script Running Success, Enjoy!")
-]]
-
--- Chạy code ở server hiện tại
-assert(loadstring(script_code))()
-
--- Đăng ký code vào hàng đợi, khi đổi game/teleport nó sẽ tự chạy lại ở server mới
-if queue_on_teleport then
-    queue_on_teleport(script_code)
-elseif syn and syn.queue_on_teleport then
-    syn.queue_on_teleport(script_code)
-end
